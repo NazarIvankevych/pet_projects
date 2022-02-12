4eaa884dd202ff97a9eeeb8b0bea5348ce65fc54
Nazar Ivankevych <fayst32567@gmail.com>
2022-02-12 21:48:35 +0200

    SMTP module
    Complete day 32 of python tutorial.

--- email_SMTP-day32/module_exampleUse.py
+++ email_SMTP-day32/module_exampleUse.py
@@ -5,12 +5,16 @@ yahoo - smtp.mail.yahoo.com
 google - smtp.gmail.com
 hotmail - smtp.live.com
 """
-my_email = "ivankevych32567@gmail.com"
-password = "xP809RC6"
-connection = smtplib.SMTP("smtp.gmail.com")
-# call TLS
-connection.starttls()
-# login
-connection.login(user=my_email, password=password)
-connection.sendmail(from_addr=my_email, to_addrs="nazar.ivankevych@yahoo.com", msg="Test out connections")
+my_email = "joker32567@gmail.com"
+password = "791yO67G"
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    # call TLS
+    connection.starttls()
+    # login
+    connection.login(user=my_email, password=password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs="nazar.ivankevych@yahoo.com",
+        msg="Subject: This is a simple mail for testing\n\nHello there!"
+    )
 
