b173c269c50d91758aeeccbde141ac8ae2317bb1
Nazar Ivankevych <fayst32567@gmail.com>
2022-03-08 18:43:12 +0200

    Add class
    Add class and create parts for main purpose to get datas and convert after describe.

--- day34-API_trivia_DB/data.py
+++ day34-API_trivia_DB/data.py
@@ -7,10 +7,24 @@ paremeters = {
 }
 
 
-response = requests.get(
-    'https://opentdb.com/api.php?amount=10&category=22&type=boolean',
-    params=paremeters
-)
-response.raise_for_status()
-data = response.json()
-question_data = data['results']
+class CollectData:
+
+    def __init__(self):
+        self.response = ""
+
+    def sendRequest(self):
+        self.response = requests.get(
+            'https://opentdb.com/api.php?amount=10&category=22&type=boolean',
+            params=paremeters
+        )
+
+    def responseReceived(self):
+        self.response.raise_for_status()
+        data = self.response.json()
+        question_data = data['results']
+        return question_data
+
+
+answers = CollectData()
+answers.sendRequest()
+print(answers.responseReceived())
