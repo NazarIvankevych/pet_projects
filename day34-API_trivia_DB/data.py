5bca63610e3c4f3bb822e62bc86ff00d20168128
Nazar Ivankevych <fayst32567@gmail.com>
2022-03-11 11:18:20 +0200

    Fix all issue with variables and classes
    Remove not needed classes and variables. Need to add all things into GUI.

--- day34-API_trivia_DB/data.py
+++ day34-API_trivia_DB/data.py
@@ -1,30 +1,14 @@
 import requests
-import json
 
 paremeters = {
     'amount': 10,
     'type': "boolean"
 }
-
-
-class CollectData:
-
-    def __init__(self):
-        self.response = ""
-
-    def sendRequest(self):
-        self.response = requests.get(
-            'https://opentdb.com/api.php?amount=10&category=22&type=boolean',
-            params=paremeters
-        )
-
-    def responseReceived(self):
-        self.response.raise_for_status()
-        data = self.response.json()
-        question_data = data['results']
-        return question_data
-
-
-answers = CollectData()
-answers.sendRequest()
-print(answers.responseReceived())
+response = requests.get(
+    'https://opentdb.com/api.php?amount=10&category=22&type=boolean',
+    params=paremeters
+)
+response.raise_for_status()
+data = response.json()
+question_data = data['results']
+# print(question_data)
--- day34-API_trivia_DB/ui.py
+++ day34-API_trivia_DB/ui.py
@@ -1,2 +1,23 @@
-THEME_COLOR = "#375362"
+from tkinter import *
+from quiz_brain import QuizBrain, check_answer
 
+
+THEME_COLOR = "#375362"
+WIDTH = 480
+HEIGHT = 360
+
+scores = QuizBrain()
+scores.check_answer(0, 0)
+
+
+class QuizInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.config(bg=THEME_COLOR, width=WIDTH, height=HEIGHT)
+        self.window.title("Trivia Game")
+        self.lbl_score = Label(
+            text=f"Score: {scores}", bg=THEME_COLOR, fg="white", font=("Arial 20"))
+        self.lbl_score.grid(column=1, row=0, columnspan=2)
+        self.lbl_text = Label(text="")
+        self.window.mainloop()
